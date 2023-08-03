97ffcbf349caec7d69bbc09b9ff1d9ecdc700375
Ivan <iaranda264@gmail.com>
2023-08-03 03:22:58 -0300

    Creando la carpeta para el componente NavBar

--- tienda/src/App.js
+++ tienda/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
