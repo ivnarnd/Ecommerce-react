6b22c9f9d3b295c8e332717163f63a83f3b3b7a3
Ivan <iaranda264@gmail.com>
2023-08-12 03:53:54 -0300

    corrigiendo items de Navbar

--- tienda/src/components/NavBar/NavBar.js
+++ tienda/src/components/NavBar/NavBar.js
@@ -4,9 +4,7 @@ function NavBar(){
     return (
         <nav class="navbar navbar-expand-lg  p-0 m-0" aria-label="Offcanvas navbar large" >
         <div className=" container-fluid">
-        <a className="navbar-brand" >
-            <h1 className="titulo-pagina">Cooking Time</h1>
-        </a>
+        <h1 className="titulo-pagina navbar-brand">Cooking Time</h1>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2"
             aria-controls="offcanvasNavbar2" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -20,16 +18,16 @@ function NavBar(){
             <div className="offcanvas-body">
                 <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                     <li className="nav-item">
-                        <a className="nav-link" aria-current="page" >Budines</a>
+                        <button>Budines</button>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" aria-current="page" >Alfajores</a>
+                        <button>Alfajores</button>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" aria-current="page" >Brownies</a>
+                        <button>Brownies</button>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" aria-current="page" ><CardWidget/></a>
+                        <CardWidget/>
                     </li>
                     
                 </ul>
