d2fbb68387af50a7858dfc0196a22f920c1264e7
Ivan <iaranda264@gmail.com>
2023-09-14 12:30:08 -0300

    creacion del cart context

--- tienda/src/App.js
+++ tienda/src/App.js
@@ -5,15 +5,18 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import CartProvider from './context/CartContext';
 function App() {
   return (
     <BrowserRouter>
-        <NavBar/>
-        <Routes>
-            <Route exact path="/" element={<ItemListContainer greeting="Bienvenidos!"/>}/>
-            <Route exact path="/category/:id" element={<ItemListContainer greeting="Bienvenidos!"/>}/>
-            <Route exact path="/item/:itemId" element={<ItemDetailContainer/>}/>
-        </Routes>
+        <CartProvider>
+          <NavBar/>
+          <Routes>
+              <Route exact path="/" element={<ItemListContainer greeting="Bienvenidos!"/>}/>
+              <Route exact path="/category/:id" element={<ItemListContainer greeting="Bienvenidos!"/>}/>
+              <Route exact path="/item/:itemId" element={<ItemDetailContainer/>}/>
+          </Routes>
+        </CartProvider>        
     </BrowserRouter>
   );
 }
--- tienda/src/context/CartContext.js
+++ tienda/src/context/CartContext.js
@@ -1,3 +1,27 @@
-import React from "react";
-const CartContext = React.createContext();
-export default CartContext;
+import React, { useState } from "react";
+const CartContext = React.createContext({cart:[]});
+const CartProvider = ({children})=>{
+    const [cart,setCart]=useState([]);
+    const isInCart = (itemId) =>{
+        return cart.some((prod)=>prod.id === itemId);
+    }
+    const addItem = (item,quantity) =>{
+        if(!isInCart(item.id)){
+            setCart(prev=>[...prev,{...item,quantity}]);
+        }
+    }
+    const removeItem = (itemId)=>{
+        const cartUpdate = cart.filter(prod=>prod.id!==itemId);
+        setCart(cartUpdate);
+    }
+    const clearCart = ()=>{
+        setCart([]);
+    }
+    return(
+        <CartContext.Provider value={{cart,addItem,removeItem,clearCart}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider;
