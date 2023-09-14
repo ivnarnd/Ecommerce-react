684fa0e639b63c7e67ba07fda54f9e50f52b2e31
Ivan <iaranda264@gmail.com>
2023-09-14 08:13:49 -0300

    configurando firebase

--- tienda/src/App.js
+++ tienda/src/App.js
@@ -5,7 +5,6 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-
 function App() {
   return (
     <BrowserRouter>
--- tienda/src/index.js
+++ tienda/src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2FtWrQmsH2ewl6lY83Wo5XLgsPxSQph4",
+  authDomain: "cooking-time-ecommerce.firebaseapp.com",
+  projectId: "cooking-time-ecommerce",
+  storageBucket: "cooking-time-ecommerce.appspot.com",
+  messagingSenderId: "283647338763",
+  appId: "1:283647338763:web:e27c1fbca4254584fc1a0e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
