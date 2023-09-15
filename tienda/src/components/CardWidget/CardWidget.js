1e934c90f490aa41bcca456d87439c47794da3ce
Ivan <iaranda264@gmail.com>
2023-09-15 07:29:48 -0300

    agregando cantidad de productos en widget de cart

--- tienda/src/components/CardWidget/CardWidget.js
+++ tienda/src/components/CardWidget/CardWidget.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+
 function CardWidget() {
+    const {totalItems} = useContext(CartContext);
     return(
         <div>
             <p>🛒</p>
-            0
+            {totalItems()}
             
         </div>
     );
