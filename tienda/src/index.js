84728fbdde8504f3694655e54484749e788c3fd0
Ivan <iaranda264@gmail.com>
2023-09-15 21:23:05 -0300

    implementando .env

--- tienda/src/index.js
+++ tienda/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -10,12 +11,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2FtWrQmsH2ewl6lY83Wo5XLgsPxSQph4",
-  authDomain: "cooking-time-ecommerce.firebaseapp.com",
-  projectId: "cooking-time-ecommerce",
-  storageBucket: "cooking-time-ecommerce.appspot.com",
-  messagingSenderId: "283647338763",
-  appId: "1:283647338763:web:e27c1fbca4254584fc1a0e"
+  apiKey: process.env.REACT_APP_apikey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
