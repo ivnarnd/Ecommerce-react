25e7628262934b239fdc9bb7327f621088b79058
Ivan <iaranda264@gmail.com>
2023-09-20 11:42:53 -0300

    limpiando los estilos de tailwind

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,24 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import CardWidget from "../CardWidget/CardWidget";
-
 function NavBar(){
     return (
-        <nav className="bg-melon flex justify-between items-center" >
-        
-                <NavLink to="/" className="link"><h1 className="p-2 text-redCMYK">Cooking Time</h1></NavLink>
-                 <ul className="flex justify-between items-center text-white">
-                    <li className="p-2">
+        <nav>
+                
+                <NavLink to="/" ><h1>Cooking Time</h1></NavLink>
+                 <ul >
+                    <li>
                         <NavLink to='/category/Repostería' className="nav-link">Reposteria</NavLink>
                     </li>
-                    <li className="p-2">
+                    <li>
                          <NavLink to='/category/Merienda' className="nav-link">Merienda</NavLink>
                     </li>
-                    <li className="p-2">
+                    <li>
                         <NavLink to='/category/Postres' className="nav-link">Postres</NavLink>
                     </li>
-                    <li className="p-2">
+                    <li>
                         <NavLink to='/category/Desayuno' className="nav-link">Desayuno</NavLink>
                     </li>   
-                    <li className="p-2">
+                    <li>
                         <CardWidget/>
                     </li>
                         
